ce8fbaa0675b05255e1cb0ab9f2dbdd8796e4cc0
orvinjedalava <ojedbub@gmail.com>
2026-06-24 11:55:32 +1000

    loading json file using AJAX fetch API

--- FrontEnd/RandomCardMemoryGame/app.js
+++ FrontEnd/RandomCardMemoryGame/app.js
@@ -1,2 +1,17 @@
 // initialize card variables
 let cards = [];
+
+loadCards();
+
+// implement the Fetch API to grab the card JSON file
+async function loadCards() {
+    try {
+        // fetch the JSON file
+        let response = await fetch("./data/card_info.json");
+        // parse the JSON file
+        let cardsArray = await response.json();
+        console.log(cardsArray);
+    } catch (error) {
+        console.log(error);
+    }
+};
