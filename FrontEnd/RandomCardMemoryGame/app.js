67288f11aa18c8915b4e8d4b5bba3cf78d707aca
orvinjedalava <ojedbub@gmail.com>
2026-06-25 10:29:57 +1000

    Creating 12 cards from array of 6 cards using Map

--- FrontEnd/RandomCardMemoryGame/app.js
+++ FrontEnd/RandomCardMemoryGame/app.js
@@ -1,6 +1,7 @@
 // initialize card variables
 let cards = [];
 
+/* using Fetch API async/await
 loadCards();
 
 // implement the Fetch API to grab the card JSON file
@@ -15,3 +16,15 @@ async function loadCards() {
         console.log(error);
     }
 };
+*/
+
+fetch("./data/card_info.json")
+    .then(response => response.json())
+    .then((data) => {
+        // OPTION 1 using MAP
+        const cardsWithMap = data.map(card => [card, card]).flat();
+        console.log(cardsWithMap);
+    })
+    .catch((error) => {
+        console.log("Error fetching card data: ", error);
+    }); 
